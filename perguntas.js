f4ac7b4abfc5ba2e124fb44d866440f8d5a2ec57
jpelepiu <jennifer.pelepiu@escola.pr.gov.br>
2025-05-26 15:02:39 -0300

    okk

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  'Orochi',
+  'Qual é a principal característica das letras do Orochi?',
+  'Orochi é conhecido por letras que misturam vivência, superação e crítica social.'
+)
+
+criaCartao(
+  'Natiruts',
+  'Qual é a vibe das músicas do Natiruts?',
+  'As músicas do Natiruts têm uma pegada de paz, natureza, cachoeiras e boas energias.'
+)
+
+criaCartao(
+  'BK',
+  'Sobre o que BK costuma rimar?',
+  'BK traz nas letras temas como resistência, vivência urbana, consciência e autoestima preta — tudo com muita potência lírica.'
+)
+
+criaCartao(
+  'Baco Exu do Blues',
+  'O que Baco Exu do Blues mistura em suas músicas?',
+  'Baco mistura rap, poesia,e temas existenciais de forma intensa.'
+)
